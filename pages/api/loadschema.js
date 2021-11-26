5184e06672bd5673a37bb30b21490d2ec32076d7
Lonnie VanZandt <lonnie.vanzandt@intercax.com>
2021-11-25 21:45:01 -0500

    try making the schema a "public" static file

--- pages/api/loadschema.js
+++ pages/api/loadschema.js
@@ -2,7 +2,7 @@ import fs from 'fs'
 
 export const typeDefs = fs
   .readFileSync(
-    process.env.GRAPHQL_SCHEMA || __dirname + 'schema.graphql'
+    process.env.GRAPHQL_SCHEMA || 'schema.graphql'
   )
   .toString('utf-8')
 
