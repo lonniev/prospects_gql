f8391f7edbe7d6f3a92a8262e28537b7dd05fc64
Lonnie VanZandt <lonnie.vanzandt@intercax.com>
2021-11-26 14:09:49 -0500

    next.js says to use process.cwd

--- pages/api/loadschema.js
+++ pages/api/loadschema.js
@@ -1,8 +1,13 @@
 import fs from 'fs'
 import { join } from 'path'
 
+// Since Next.js compiles your code into a separate directory
+// you can't use __dirname as the path it will return will be different from the pages directory.
+// Instead you can use process.cwd() 
+// which gives you the directory where Next.js is being executed.
+
 export const typeDefs = fs
   .readFileSync(
-    join( __dirname, 'schemas', 'schema.graphql' )
+    join( process.cwd(), 'schemas', 'schema.graphql' )
   )
   .toString('utf-8')
